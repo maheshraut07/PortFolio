4ded75cba310d04a555d4a765dea77bea364ee94
maheshraut07 <rautmahesh213@gmail.com>
2024-10-14 14:02:10 +0530

    added the Navbar, HeroSection and the About sectio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import aboutImg from '../assets/MaheshProfileInOffice.jpg';
-import {ABOUT_TEXT} from '../constants/index.js'
+import React from "react";
+import aboutImg from "../assets/MaheshProfileInOffice.jpg";
+import { ABOUT_TEXT } from "../constants/index.js";
 
 const About = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4'>
-      <h1 className='my-20 text-center text-4xl'>
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">
         About
-        <span className='text-neutral-500'> Me</span>
+        <span className="text-neutral-500"> Me</span>
       </h1>
-      <div className='flex flex-wrap'>
-        <div className='w-full lg:w-1/2 lg:p-8'>
-          <div className='flex items-center justify-center'>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex items-center justify-center">
             {/* Set the image size, object fit, and rounded corners */}
-            <img 
-              src={aboutImg} 
-              alt="aboutProfile" 
-              className='h-auto w-64 max-h-80 rounded-lg object-cover shadow-lg' 
+            <img
+              src={aboutImg}
+              alt="aboutProfile"
+              className="h-auto w-64 max-h-80 rounded-lg object-cover shadow-lg"
             />
           </div>
-          <div className='w-full lg:w-1/2'>
-            <div className='flex justify-center lg:justify-start'>
-                <p>{ABOUT_TEXT}</p>
-            </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 maxw-xl py-6">{ABOUT_TEXT}</p>
           </div>
         </div>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-14 lg:text-8xl">
               Mahesh Raut
             </h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
@@ -22,7 +22,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2 lg:p-5">
           <div className="flex justify-center items-center h-full">
             <img
-              className="h-auto w-80 max-h-100 object-cover rounded-lg" 
+              className="h-auto w-80 mt-4 max-h-100 object-cover rounded-lg" 
               src={profilePic} 
               alt="profilePic" 
             />

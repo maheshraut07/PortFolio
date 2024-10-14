39d9729e7434d8f92df058dbb1bddcd712f8502a
maheshraut07 <rautmahesh213@gmail.com>
2024-10-14 11:24:11 +0530

    all files are uploaded

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/MaheshRautLogo.jpg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mx-2 mb-20 flex items-center justify-between py-5">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="w-9" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitterSquare />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

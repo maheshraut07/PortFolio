216f6ab4cb8f36b270ac918c28b8d47407eecf47
maheshraut07 <rautmahesh213@gmail.com>
2024-10-15 21:04:42 +0530

    added the framer motion animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { PROJECTS } from "../constants";
+import {motion} from 'framer-motion'
+
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl">Projects</h1>
+      <motion.h1 
+         whileInView={{opacity: 1, y:0}}
+         initial={{opacity: 0, y: -100}}
+         transition={{duration:0.5}}
+        className="my-20 text-center text-4xl">Projects</motion.h1>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div 
+              whileInView={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: -100}}
+              transition={{duration: 1}}
+              className="w-full lg:w-1/4">
               <img
                 src={project.image}
                 width={350}
@@ -16,8 +26,12 @@ const Projects = () => {
                 alt={project.title}
                 className="mb-8 rounded"
               />
-            </div>
-            <div className="w-full mr-3 max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div 
+              whileInView={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: 100}}
+              transition={{duration: 1.5}}
+              className="w-full mr-3 max-w-xl lg:w-3/4">
               <div className="flex flex-wrap gap-5">
                 <span className="mb-1 font-semibold">{project.title}</span>
                 <span className="mb-2">{project.duration}</span>
@@ -32,7 +46,7 @@ const Projects = () => {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -9,43 +9,106 @@ import { SiTailwindcss } from 'react-icons/si';
 import { TbBrandRedux } from 'react-icons/tb';
 import { BsGithub } from 'react-icons/bs';
 import { DiDocker } from 'react-icons/di';
+import { motion } from "framer-motion";
 
-const Technology = () => {
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10,-10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
+
+
+const Technology = () => { 
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h1 className='my-20 text-center text-4xl'>Technologies</h1>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.h1 
+            whileInView={{opacity: 1, y: 0}}
+            initial={{opacity: 0, y: -100}}
+            transition={{duration: 1.5}}
+            className='my-20 text-center text-4xl'>Technologies</motion.h1>
+        <motion.div 
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: -100}}
+            transition={{duration: 1.5}}
+            className='flex flex-wrap items-center justify-center gap-4'>
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandNextjs className='text-7xl font-bold'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaNodeJs className='text-7xl text-green-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiExpress className='text-7xl'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMongodb className='text-7xl text-green-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiMysql className='text-7xl text-orange-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(5.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiTailwindcss className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(1.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandRedux className='text-7xl text-violet-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <BsGithub className='text-7xl'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(5.5)}
+                initial="initial"
+                animate="animate" 
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiDocker className='text-7xl text-blue-400'/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }

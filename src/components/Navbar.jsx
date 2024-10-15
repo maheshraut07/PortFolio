9f6c761795796fd1e106e89823bc436e2b0c16f1
maheshraut07 <rautmahesh213@gmail.com>
2024-10-15 21:34:40 +0530

    resume link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,20 +10,13 @@ const Navbar = () => {
     <nav className="mb-20 flex items-center justify-between py-5">
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-10" src={logo} alt="logo" />
-        {/* <div className="flex gap-3">
-        <span>Home</span>
-        <span>About</span>
-        <span>TechStack</span>
-        <span>Experience</span>
-        <span>Projects</span>
-        <span>Connect</span>
-        </div> */}
+        <a className="font-semibold underline text-purple-500" href="https://drive.google.com/file/d/1Il6DC5dzLqwvIcQkN2PHTBTA1ypnTt6v/view?usp=sharing" target="_blank">Resume</a>
       </div>
       <div className="m-8 flex flex-wrap items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaTwitterSquare />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/mahesh-raut-96aa59228/" target="_blank"><FaLinkedin /></a>
+        <a href="https://github.com/maheshraut07" target="_blank"><FaGithub /></a>
+        <a href="https://x.com/maheshraut_07" target="_blank"><FaTwitterSquare /></a>
+        <a href="https://www.instagram.com/maheshraut_07/" target="_blank"><FaInstagram /></a>
       </div>
     </nav>
   );

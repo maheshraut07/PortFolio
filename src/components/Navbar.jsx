0397d620622d309f61848009d2187154ea306bb9
maheshraut07 <rautmahesh213@gmail.com>
2024-10-23 12:16:41 +0530

    updated the navbar styling and added the 4 more technologies in the technology component in the react

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import logo from "../assets/MaheshRautLogo.jpg";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaTwitterSquare } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTwitterSquare, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-5">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
-        <a className="font-semibold underline text-purple-500" href="https://drive.google.com/file/d/1Il6DC5dzLqwvIcQkN2PHTBTA1ypnTt6v/view?usp=sharing" target="_blank">Resume</a>
+      {/* Logo and Resume */}
+      <div className="flex items-center space-x-4">
+        <img className="w-8 sm:w-10" src={logo} alt="logo" />
+        <a
+          className="text-sm sm:text-base font-semibold underline text-purple-500"
+          href="https://drive.google.com/file/d/1Il6DC5dzLqwvIcQkN2PHTBTA1ypnTt6v/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
       </div>
-      <div className="m-8 flex flex-wrap items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/mahesh-raut-96aa59228/" target="_blank"><FaLinkedin /></a>
-        <a href="https://github.com/maheshraut07" target="_blank"><FaGithub /></a>
-        <a href="https://x.com/maheshraut_07" target="_blank"><FaTwitterSquare /></a>
-        <a href="https://www.instagram.com/maheshraut_07/" target="_blank"><FaInstagram /></a>
+      
+      {/* Social Icons */}
+      <div className="flex items-center space-x-3 sm:space-x-4 text-xl sm:text-2xl">
+        <a href="https://www.linkedin.com/in/mahesh-raut-96aa59228/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/maheshraut07" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://x.com/maheshraut_07" target="_blank" rel="noopener noreferrer">
+          <FaTwitterSquare />
+        </a>
+        <a href="https://www.instagram.com/maheshraut_07/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -9,6 +9,10 @@ import { SiTailwindcss } from 'react-icons/si';
 import { TbBrandRedux } from 'react-icons/tb';
 import { BsGithub } from 'react-icons/bs';
 import { DiDocker } from 'react-icons/di';
+import { DiPython } from 'react-icons/di';
+import { TbBrandCpp } from "react-icons/tb";
+import { DiJava } from 'react-icons/di';
+import { BiLogoGoLang } from 'react-icons/bi';
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -33,11 +37,49 @@ const Technology = () => {
             initial={{opacity: 0, y: -100}}
             transition={{duration: 1.5}}
             className='my-20 text-center text-4xl'>Technologies</motion.h1>
-        <motion.div 
+
+            <motion.div 
             whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: -100}}
             transition={{duration: 1.5}}
             className='flex flex-wrap items-center justify-center gap-4'>
+
+            {/* Python */}
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <DiPython className='text-7xl' style={{color: '#3776AB'}}/>
+            </motion.div>
+
+            {/* C++ */}
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <TbBrandCpp className='text-7xl' style={{color: '#00599C'}}/>
+            </motion.div>
+
+            {/* Java */}
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <DiJava className='text-7xl' style={{color: '#f89820'}}/>
+            </motion.div>
+
+            {/* GoLang */}
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <BiLogoGoLang className='text-7xl' style={{color: '#00ADD8'}}/>
+            </motion.div>
+            
             <motion.div 
                 variants={iconVariants(2.5)}
                 initial="initial"
@@ -45,6 +87,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(3)}
                 initial="initial"
@@ -52,6 +95,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandNextjs className='text-7xl font-bold'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(3.5)}
                 initial="initial"
@@ -59,6 +103,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaNodeJs className='text-7xl text-green-500'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(4)}
                 initial="initial"
@@ -66,6 +111,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiExpress className='text-7xl'/>
             </motion.div>
+            
             <motion.div 
                 variants={iconVariants(4.5)}
                 initial="initial"
@@ -73,6 +119,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMongodb className='text-7xl text-green-500'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(1.5)}
                 initial="initial"
@@ -80,6 +127,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiMysql className='text-7xl text-orange-400'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(5.5)}
                 initial="initial"
@@ -87,6 +135,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiTailwindcss className='text-7xl text-cyan-400'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(1.5)}
                 initial="initial"
@@ -94,6 +143,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandRedux className='text-7xl text-violet-500'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(3.5)}
                 initial="initial"
@@ -101,6 +151,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <BsGithub className='text-7xl'/>
             </motion.div>
+
             <motion.div 
                 variants={iconVariants(5.5)}
                 initial="initial"
@@ -108,6 +159,7 @@ const Technology = () => {
                 className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiDocker className='text-7xl text-blue-400'/>
             </motion.div>
+
         </motion.div>
     </div>
   )

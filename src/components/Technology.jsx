117a93451038190113a650c03831d8a919dfd626
maheshraut07 <rautmahesh213@gmail.com>
2024-10-23 12:23:34 +0530

    updated the navbar styling and added the 4 more technologies in the technology component in the react

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,168 +1,204 @@
-import React from 'react'
-import { RiReactjsLine } from 'react-icons/ri'
-import { TbBrandNextjs } from 'react-icons/tb';
-import { FaNodeJs } from 'react-icons/fa';
-import { SiExpress } from 'react-icons/si';
-import { SiMongodb } from 'react-icons/si';
-import { DiMysql } from 'react-icons/di';
-import { SiTailwindcss } from 'react-icons/si';
-import { TbBrandRedux } from 'react-icons/tb';
-import { BsGithub } from 'react-icons/bs';
-import { DiDocker } from 'react-icons/di';
-import { DiPython } from 'react-icons/di';
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { DiMysql } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
+import { TbBrandRedux } from "react-icons/tb";
+import { BsGithub } from "react-icons/bs";
+import { DiDocker } from "react-icons/di";
+import { DiPython } from "react-icons/di";
 import { TbBrandCpp } from "react-icons/tb";
-import { DiJava } from 'react-icons/di';
-import { BiLogoGoLang } from 'react-icons/bi';
+import { DiJava } from "react-icons/di";
+import { BiLogoGoLang } from "react-icons/bi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiPostman } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-    initial: {y: -10},
-    animate: {
-        y: [10,-10],
-        transition: {
-            duration: duration,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "reverse",
-        }
-    }
-})
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
-
-const Technology = () => { 
+const Technology = () => {
   return (
-    <div className='border-b border-neutral-800 pb-24'>
-        <motion.h1 
-            whileInView={{opacity: 1, y: 0}}
-            initial={{opacity: 0, y: -100}}
-            transition={{duration: 1.5}}
-            className='my-20 text-center text-4xl'>Technologies</motion.h1>
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
 
-            <motion.div 
-            whileInView={{opacity: 1, x: 0}}
-            initial={{opacity: 0, x: -100}}
-            transition={{duration: 1.5}}
-            className='flex flex-wrap items-center justify-center gap-4'>
-
-            {/* Python */}
-            <motion.div 
-                variants={iconVariants(2.5)}
-                initial="initial"
-                animate="animate"
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiPython className='text-7xl' style={{color: '#3776AB'}}/>
-            </motion.div>
-
-            {/* C++ */}
-            <motion.div 
-                variants={iconVariants(2.5)}
-                initial="initial"
-                animate="animate"
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <TbBrandCpp className='text-7xl' style={{color: '#00599C'}}/>
-            </motion.div>
-
-            {/* Java */}
-            <motion.div 
-                variants={iconVariants(2.5)}
-                initial="initial"
-                animate="animate"
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiJava className='text-7xl' style={{color: '#f89820'}}/>
-            </motion.div>
-
-            {/* GoLang */}
-            <motion.div 
-                variants={iconVariants(2.5)}
-                initial="initial"
-                animate="animate"
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <BiLogoGoLang className='text-7xl' style={{color: '#00ADD8'}}/>
-            </motion.div>
-            
-            <motion.div 
-                variants={iconVariants(2.5)}
-                initial="initial"
-                animate="animate"
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(3)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <TbBrandNextjs className='text-7xl font-bold'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(3.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaNodeJs className='text-7xl text-green-500'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(4)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiExpress className='text-7xl'/>
-            </motion.div>
-            
-            <motion.div 
-                variants={iconVariants(4.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiMongodb className='text-7xl text-green-500'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(1.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiMysql className='text-7xl text-orange-400'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(5.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiTailwindcss className='text-7xl text-cyan-400'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(1.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <TbBrandRedux className='text-7xl text-violet-500'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(3.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <BsGithub className='text-7xl'/>
-            </motion.div>
-
-            <motion.div 
-                variants={iconVariants(5.5)}
-                initial="initial"
-                animate="animate" 
-                className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiDocker className='text-7xl text-blue-400'/>
-            </motion.div>
-
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        {/* Python */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiPython className="text-7xl" style={{ color: "#3776AB" }} />
         </motion.div>
-    </div>
-  )
-}
 
-export default Technology
+        {/* C++ */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandCpp className="text-7xl" style={{ color: "#00599C" }} />
+        </motion.div>
+
+        {/* Java */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiJava className="text-7xl" style={{ color: "#f89820" }} />
+        </motion.div>
+
+        {/* JavaScript */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoJavascript className="text-7xl" style={{ color: "#f7df1e" }} />
+        </motion.div>
+
+        {/* GoLang */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoGoLang className="text-7xl" style={{ color: "#00ADD8" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandNextjs className="text-7xl font-bold" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiExpress className="text-7xl" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiMysql className="text-7xl text-orange-400" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTailwindcss className="text-7xl text-cyan-400" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandRedux className="text-7xl text-violet-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BsGithub className="text-7xl" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPostman className="text-7xl text-orange-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiDocker className="text-7xl text-blue-400" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technology;
